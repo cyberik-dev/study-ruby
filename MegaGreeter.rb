feea06a756d06ada03614a345c9740c5851f2cee
Cyberik <cyberik@ya.ru>
2016-09-15 17:35:06 +0300

    add class MegaGreeter

---
+++ MegaGreeter.rb
@@ -0,0 +1,52 @@
+class MegaGreeter
+	attr_accessor :names
+
+	# constructor
+	def initialize(names = 'World')
+		@names = names
+	end
+
+	# say hi
+	def say_hi
+		if @names.nil?
+			puts '...'
+		elsif @names.respond_to?('each')
+			# @names is a list of some kind
+			@names.each do |name|
+				puts "Hello #{name}!" 
+			end
+		else
+			puts "Hello #{@names}!"
+		end
+	end
+
+	# say bay to everybody
+	def say_bye
+		if @names.nil?
+			puts "..."
+		elsif @names.respond_to?("join")
+			# join the list elements
+			puts "Goodbye #{@names.join(", ")}. Come back soon!"
+		else
+			puts "Goodbye #{@names}. Come back soon!"
+		end
+	end
+
+end
+
+mg = MegaGreeter.new
+mg.say_hi
+mg.say_bye
+
+mg.names = 'Vova'
+mg.say_hi
+mg.say_bye
+
+
+mg.names = ['Vasia', 'Kola', 'Marina', 'Viktor']
+mg.say_hi
+mg.say_bye
+
+mg.names = nil
+mg.say_hi
+mg.say_bye
